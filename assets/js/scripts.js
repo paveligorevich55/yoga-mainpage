de22ab478ee53b6f59e1b12e775c60bfad20fb9e
Pavlo Konchych <pavlo.konchych@gmail.com>
2018-12-10 02:05:50 +0200

    secondary commit
    Not responsive and not adaptive

---
+++ assets/js/scripts.js
@@ -0,0 +1,108 @@
+
+$(document).ready( function () {   
+	
+    $('.next').on('click', function(){
+        var currentImage = $('.about-item.current'),
+         	currentImageIndex = $('.about-item.current').index(),
+		 	nextImageIndex = currentImageIndex + 1,
+		 	nextImage = $('.about-item').eq(nextImageIndex);
+        currentImage.fadeOut(0);
+        currentImage.removeClass('current');
+
+        if(nextImageIndex == ($('.about-item:last').index()+1)){
+            $('.about-item').eq(0).fadeIn(1000);
+            $('.about-item').eq(0).addClass('current');
+        } else {
+            nextImage.fadeIn(1000);
+            nextImage.addClass('current');
+        }
+    });
+
+    $('.prev').on('click', function(){
+        var currentImage = $('.about-item.current'),
+            currentImageIndex = $('.about-item.current').index(),
+            prevImageIndex = currentImageIndex - 1,
+            prevImage = $('.about-item').eq(prevImageIndex);
+        
+        currentImage.fadeOut(0);
+        currentImage.removeClass('current');
+        prevImage.fadeIn(1000);
+        prevImage.addClass('current');
+    })
+    var buyNow = $('.buy-now'),
+   		cardHeading = $('.card-heading'),
+   		card = $('.card');
+    
+	buyNow.mouseover( function (){
+	  $(this).closest(card).find(cardHeading).css('background-color', 'red');
+	  $(this).closest(card).find(cardHeading).css('border', '1px solid red');
+	});
+	
+	buyNow.mouseleave( function (){
+	  $(this).closest(card).find(cardHeading).css('background-color', 'rgb(83, 99, 219)');
+	  $(this).closest(card).find(cardHeading).css('border', '1px solid rgb(83, 99, 219)');
+	});
+
+	(function ($){
+		jQuery.fn.quoteTabs = function(options){
+
+			var createTabs = function(){
+				tabs = this;
+				i = 0;
+
+				showPage = function (i){
+					$(tabs).children('div').children('div').hide();
+					$(tabs).children('div').children('div').eq(i).show();
+					$(tabs).children('ul').children('li').removeClass('active');
+					$(tabs).children('ul').children('li').eq(i).addClass('active');
+				}
+
+				showPage(0);
+
+				$(tabs).children('ul').children('li').each(function (index, element){
+					$(element).attr("data-page", i);
+					i++;
+				});
+
+				$(tabs).children('ul').children('li').on('click', function(){
+					showPage(parseInt($(this).attr('data-page')));
+				});
+			};
+			return this.each(createTabs);
+		};
+	})(jQuery);
+
+	$('.quotes').quoteTabs();
+	
+	
+	$('.qoute-trash-button.next').on('click', function(){
+        var currentAuthor = $('.quote-author.current'),
+         	currentAuthorIndex = $('.quote-author.current').index(),
+		 	nextAuthorIndex = currentAuthorIndex + 1,
+		 	nextAuthor = $('.quote-author').eq(nextAuthorIndex);
+        currentAuthor.fadeOut(0);
+        currentAuthor.removeClass('current');
+
+        if(nextAuthorIndex == ($('.quote-author:last').index()+1)){
+            $('.quote-author').eq(0).fadeIn(1000);
+            $('.quote-author').eq(0).addClass('current');
+        } else {
+            nextAuthor.fadeIn(1000);
+            nextAuthor.addClass('current');
+        }
+    });
+
+    $('.qoute-trash-button.prev').on('click', function(){
+        var currentAuthor = $('.quote-author.current'),
+            currentAuthorIndex = $('.quote-author.current').index(),
+            prevAuthorIndex = currentAuthorIndex - 1,
+            prevAuthor = $('.quote-author').eq(prevAuthorIndex);
+        
+        currentAuthor.fadeOut(0);
+        currentAuthor.removeClass('current');
+        prevAuthor.fadeIn(1000);
+        prevAuthor.addClass('current');
+    })
+
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,25 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    cssnano = require('gulp-cssnano'), // Минимизация CSS
-    autoprefixer = require('gulp-autoprefixer'), // Проставлет вендорные префиксы в CSS для поддержки старых браузеров
-    rename = require('gulp-rename'); // Переименование файлов
-    
-/* --------------------------------------------------------
-   ----------------- Таски ---------------------------
------------------------------------------------------------- */
+    cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'), 
+    rename = require('gulp-rename'),
+    minify = require('gulp-minify');
+ 
+	// Минимизация JS файлов
+	gulp.task('compress', function() {
+	  gulp.src(['assets/js/*.js', 'assets/js/*.mjs'])
+	    .pipe(minify())
+	    .pipe(gulp.dest('assets/js/min/'))
+	});
 
 	// Копирование файлов HTML в папку dist
 	gulp.task('html', function() {
 	    return gulp.src('*.html')
 	});
 	
-	// Объединение, компиляция Sass в CSS, простановка венд. префиксов и дальнейшая минимизация кода
+	// Объединение, компиляция Sass в CSS, 
+	//простановка венд. префиксов и дальнейшая минимизация кода
 	gulp.task('sass', function(){
 	    return gulp.src(['assets/scss/**/*.scss'])
 	    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
@@ -28,12 +33,15 @@ var gulp = require('gulp'),
 	    .pipe(gulp.dest('assets/css'))
 	});
 	
-	
+	// Выбираем файл для минификации
+	// Сжимаем
+	// Добавляем суффикс .min
+	// Выгружаем в папку 
 	gulp.task('css-libs', ['sass'], function() {
-	    return gulp.src('assets/css/styles.css') // Выбираем файл для минификации
-	        .pipe(cssnano()) // Сжимаем
-	        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-	        .pipe(gulp.dest('assets/css/min/')); // Выгружаем в папку 
+	    return gulp.src('assets/css/styles.css') 
+	        .pipe(cssnano()) 
+	        .pipe(rename({suffix: '.min'})) 
+	        .pipe(gulp.dest('assets/css/min/')); 
 	});
 	
 	
@@ -42,8 +50,7 @@ var gulp = require('gulp'),
 	    gulp.watch(['assets/scss/**/*.scss'], ['sass']);
 	    gulp.watch('*.html', ['html']);
 	});
-	///// Таски ///////////////////////////////////////
 
-// Запуск тасков по умолчанию
+	// Запуск тасков по умолчанию
 
-gulp.task('default', ['watch','html', 'sass', 'css-libs']);
+gulp.task('default', ['watch','html', 'sass', 'css-libs', 'compress']);
